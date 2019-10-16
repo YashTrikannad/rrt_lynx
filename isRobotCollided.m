2b886b2c14bb6ba2ab46643ecb84bc2aa7524f70
Preminator <vishnu.prem06@gmail.com>
2019-10-15 21:39:17 -0400

    isRobotCollided complete

--- isRobotCollided.m
+++ isRobotCollided.m
@@ -14,6 +14,37 @@ function [isCollided] = isRobotCollided(q, map, robot)
 %%%                  Algortihm Starts Here             %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% q = [0 0 0 0 0 0];
+% map = loadmap('example_map.txt');
+% load('robot.mat')
+
+%get x,y,z for all joints of lynx robot at configuration q
+joint_pos = calculateFK_sol(q);
+
+%end points of 5 line segments representing the 5 links
+linePt1 = joint_pos(1:5, :);
+linePt2 = joint_pos(2:6, :);
+
+%number of obstacles in map
+obstacles_size = size(map.obstacles);
+obstacles_num = obstacles_size(1);
+
+%initially no collisions
+isCollided = 0;
+
+%looping through all obstacles
+for i=1:obstacles_num 
+    
+    %array showing if any link collides with obstacle
+    result = detectCollision(linePt1, linePt2, map.obstacles(i,:));
+    
+    %to check if any link collided with obstacle
+    if any(result) == 1
+        isCollided = 1;
+        break
+    end
+end
+
 
     
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
